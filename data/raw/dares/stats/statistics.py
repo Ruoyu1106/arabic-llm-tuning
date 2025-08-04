e806633d61a25841e5b794e39ecc88cff5bfb495
ruoyu1106 <rzha901@aucklanduni.ac.nz>
2025-08-04 16:17:53 +1200

    update data

---
+++ data/raw/dares/stats/statistics.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+# dataset = pd.read_csv('data/dataset.csv')
+#
+# # print(dataset)
+#
+# groups = dataset.groupby('Grade').size()
+# print(groups)
+
+# Initial dataset
+# data = pd.read_excel('data/readability/raw_data.xlsx')
+# groups = data.groupby('Grade').size()
+# print(groups)
+#
+# print("Stats for training set")
+# data = pd.read_csv('data/readability/train.csv', sep='\t')
+#
+# print(f'training samples {len(data)}')
+#
+# groups = data.groupby('Grade').size()
+# print(groups)
+#
+# print("Stats for test set")
+# data = pd.read_csv('data/readability/test.csv', sep='\t')
+# groups = data.groupby('Grade').size()
+# print(groups)
+#
+# print("Stats for validation set")
+# data = pd.read_csv('data/readability/validation.csv', sep='\t')
+# groups = data.groupby('Grade').size()
+# print(groups)
+
+# print("Stats for all")
+# data = pd.read_excel('data/readability/raw/categorised_raw_data.xlsx')
+# groups = data.groupby('Grade').size()
+# print(groups)
+#
+#
+# print("Stats for training set")
+# data = pd.read_csv('data/readability/raw/train.csv', sep='\t')
+# groups = data.groupby('Grade').size()
+# print(groups)
+#
+# print("Stats for test set")
+# data = pd.read_csv('data/readability/raw/test.csv', sep='\t')
+# groups = data.groupby('Grade').size()
+# print(groups)
+#
+# print("Stats for validation set")
+# data = pd.read_csv('data/readability/raw/validation.csv', sep='\t')
+# groups = data.groupby('Grade').size()
+# print(groups)
+
+# Initial dataset
+data = pd.read_csv('data/readability/extended/train.csv',sep='\t')
+# data = pd.read_csv('data/readability/raw/extended/test.csv',sep='\t')
+groups = data.groupby('Arabic_Filename')
+
+for key in groups.groups.keys():
+    group_df = groups.get_group(key)
+    sub_group = group_df.groupby('Grade').size()
+    print(f'=================={key}============================')
+    print(sub_group)
---
+++ data/raw/dares/utils/arguments.py
@@ -0,0 +1,19 @@
+from simpletransformers.config.model_args import ClassificationArgs
+
+
+def get_arguments():
+    model_args = ClassificationArgs()
+    model_args.use_multiprocessing = False
+    model_args.use_multiprocessing_for_evaluation = False
+    model_args.save_best_model = False
+    model_args.save_eval_checkpoints = False
+    model_args.save_model_every_epoch = False
+    model_args.evaluate_during_training = True
+    model_args.evaluate_during_training_steps = 150
+    model_args.evaluate_during_training_verbose = True
+    model_args.overwrite_output_dir = True
+    model_args.logging_steps = 150
+    # model_args.save_steps = 150
+    model_args.max_seq_length = 512
+    model_args.wandb_project = 'arabic-readability-assessment'
+    return model_args
